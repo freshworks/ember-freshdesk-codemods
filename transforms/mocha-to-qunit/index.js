379c8ec4e614747635025005aca69c37ccfdfaaa
Parsuram Vijayasankar <parsuram.vijaysankar@freshworks.com>
2019-11-15 12:13:53 +0530

    Correcting the setupAcceptance method

--- transforms/mocha-to-qunit/index.js
+++ transforms/mocha-to-qunit/index.js
@@ -23,7 +23,9 @@ const {
 // [ ] Take a sample batch of 50 and migrate them to qunit using some simple code-mods.
 // [X] Refactor to a mapper to determine what type of assertion and what type of transform.
 // [X] Clean up unused imports sych as context, findAll.
+// [ ] There are some tests which have an expect as the return statements and also having await methods infront of it.
 // [ ] Migrate context to module and add hooks.
+// [ ] If a describe or a context is skipped then all the tests within the specifc module needs to be skipped and added as an import to qunit
 // [ ] Clean up any beforeEach and afterEach called directly form mocha.
 
 module.exports = function transformer(file, api) {
@@ -37,7 +39,7 @@ module.exports = function transformer(file, api) {
     ['context', 'module'],
     ['it', 'test'],
     ['setupRenderingWithMirage', 'setupRenderingForModule'],
-    ['setupAcceptance', 'setupAcceptanceForModule']
+    ['setupAcceptance', 'setupApplicationForModule']
   ];
 
   const renameImportImports = [
