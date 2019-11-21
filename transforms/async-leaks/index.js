b80eee152ef0b1eb07199362e79ed65a8b814f92
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-11-21 09:52:38 +0530

    Optional import

--- transforms/async-leaks/index.js
+++ transforms/async-leaks/index.js
@@ -1,16 +1,26 @@
-const { getParser } = require('codemod-cli').jscodeshift;
-const { getOptions } = require('codemod-cli');
+const { getParser } = require("codemod-cli").jscodeshift;
+const { getOptions } = require("codemod-cli");
 
 module.exports = function transformer(file, api) {
   const j = getParser(api);
   const options = getOptions();
-
   const root = j(file.source);
+  let isModified = false;
+
+  const importRun = root.find(j.ImportDeclaration, {
+    source: {
+      value: "@ember/runloop"
+    }
+  });
+  const isImportExists = importRun && importRun.length > 0;
 
   root
     .find(j.CallExpression)
     .filter(path => {
-      return path.node.arguments.length === 1 && path.node.arguments[0].value == "store";
+      return (
+        path.node.arguments.length === 1 &&
+        path.node.arguments[0].value == "store"
+      );
     })
     .closest(j.ExpressionStatement)
     .replaceWith(nodePath => {
@@ -21,8 +31,17 @@ module.exports = function transformer(file, api) {
           j.arrowFunctionExpression([], j.blockStatement([node]))
         ])
       );
+      isModified = true;
       return newNode;
     });
 
+  if (isModified && !isImportExists) {
+    const importStatement = j.importDeclaration(
+      [j.importSpecifier(j.identifier("run"), j.identifier("run"))],
+      j.literal("@ember/runloop")
+    );
+    root.find(j.ImportDeclaration).insertAfter(importStatement);
+  }
+
   return root.toSource();
-}
+};
