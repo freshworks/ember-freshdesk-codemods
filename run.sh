e6b04863845ff731d8f380cd3f2022734da45b42
Parsuram Vijayasankar <parsuram.vijaysankar@freshworks.com>
2019-12-03 16:51:07 +0530

    Clearing the shell command

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+echo 'Running base tranforms'
 ./bin/cli.js mocha-to-qunit $1
-./bin/cli.js setup-for-custom-assertions $1
+
+echo 'Running qunit-dom tranforms'
 jscodeshift -t https://raw.githubusercontent.com/simplabs/qunit-dom-codemod/master/qunit-dom-codemod.js $1
+
+echo 'Cleaning up additional qunit-dom tranforms'
 ./bin/cli.js cleanup-imports $1
+
+echo 'Running async leaks'
+./bin/cli.js async-leaks $1
