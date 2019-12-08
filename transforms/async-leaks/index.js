e36b6183d9279ea06780c23f297d53d81eec1671
SandeepJoelFreshdesk <sandeepjoel@freshdesk.com>
2019-12-08 22:52:04 +0530

    Group all store operations (expressions and declarations)

--- transforms/async-leaks/index.js
+++ transforms/async-leaks/index.js
@@ -5,95 +5,146 @@ const beautifyImports = require("../beautify-imports");
 module.exports = function transformer(file, api) {
   const j = getParser(api);
   const options = getOptions();
-  const root = j(file.source);
+  const firstRoot = j(file.source);
   const lineTerminator = file.source.indexOf('\r\n') > -1 ? '\r\n' : '\n';
   let isModified = false;
-  let groupedNodes;
-  const importRun = root.find(j.ImportDeclaration, {
+  const importRun = firstRoot.find(j.ImportDeclaration, {
     source: {
       value: "@ember/runloop"
     }
   });
   const isImportExists = importRun && importRun.length > 0;
 
-  const blocks = root.find(j.BlockStatement).filter((path) => {
-    return ['it', 'after', 'before', 'afterEach', 'beforeEach']
-      .includes(path.parent.parent.value.callee.name || path.parent.parent.value.callee.property.name)
-  });
-
-  blocks.forEach((block) => {
-    groupedNodes = [];
-    const leakingExpression = j(block)
-      .find(j.CallExpression)
-      .filter(path => {
-        return (
-          path.node.arguments.length &&
-          path.node.arguments.some(n => n.value == "store")
-        );
-      })
-      .closest(j.ExpressionStatement)
-      .filter(path => {
-        // check for existing run loop
-        return path.parent.parent.parent.node.callee && path.parent.parent.parent.node.callee.name != "run";
-      });
-
-    // GROUP ADJACENT
-    const groupedExp = leakingExpression
-      .filter((nodePath, index, exp) => {
-        const currentNode = nodePath.value;
-        const prevNode = exp[index - 1] ? exp[index - 1].value : null;
-        const nextNode = exp[index + 1] ? exp[index + 1].value : null;
-        return (currentNode.start == (prevNode && prevNode.end + 5) || currentNode.end + 5 == (nextNode && nextNode.start));
-      });
-
-    groupedExp.length && groupedExp.forEach(nodePath => {
-      const { node } = nodePath;
-      groupedNodes.push(node);
-      isModified = true;
-    });
-
-    const newNodes = j.expressionStatement(
-      j.callExpression(j.identifier("run"), [j.arrowFunctionExpression([], j.blockStatement([...groupedNodes]))])
-    );
-    groupedExp.remove();
-    groupedExp && groupedNodes.length && groupedExp.get().insertAfter(newNodes);
-
-    // FILTER SOLO
-    const soloExp = leakingExpression
-      .filter((nodePath, index, exp) => {
-        const currentNode = nodePath.value;
-        const prevNode = exp[index - 1] ? exp[index - 1].value : null;
-        const nextNode = exp[index + 1] ? exp[index + 1].value : null;
-        return (currentNode && !(currentNode.start == (prevNode && prevNode.end + 5) || currentNode.end + 5 == (nextNode && nextNode.start)));
-      });
-
-    soloExp.length && soloExp.replaceWith(nodePath => {
-      const { node } = nodePath;
-      // wrap with run
-      const newNode = j.expressionStatement(
-        j.callExpression(j.identifier("run"), [
-          j.arrowFunctionExpression([], j.blockStatement([node]))
-        ])
+  const storeExpressionsNotInRunLoop = firstRoot
+    .find(j.CallExpression)
+    .filter(path => {
+      return (
+        path.node.arguments.length &&
+        path.node.arguments.some(n => n.value == "store")
       );
-      isModified = true;
-      return newNode;
+    })
+    .closest(j.ExpressionStatement)
+    .filter(path => {
+      // check for existing run loop
+      return (path.parent.parent.parent.node.callee && !["run", "describe"].includes(path.parent.parent.parent.node.callee.name));
     });
 
+  storeExpressionsNotInRunLoop.length && storeExpressionsNotInRunLoop.replaceWith(nodePath => {
+    const { node } = nodePath;
+    // wrap with run
+    const newNode = j.expressionStatement(
+      j.callExpression(j.identifier("run"), [
+        j.arrowFunctionExpression([], j.blockStatement([node]))
+      ])
+    );
+    isModified = true;
+    return newNode;
   });
+
+  const storeDeclarationsNotInRunLoop = firstRoot
+    .find(j.CallExpression)
+    .filter(path => {
+      return (
+        path.node.arguments.length &&
+        path.node.arguments.some(n => n.value == "store")
+      );
+    })
+    .closest(j.VariableDeclaration)
+    .filter(path => {
+      // check for existing run loop    
+      return (path.parent.parent.parent.node.callee && !["run", "describe"].includes(path.parent.parent.parent.node.callee.name));
+    });;
+
+  storeDeclarationsNotInRunLoop.length && storeDeclarationsNotInRunLoop.replaceWith(nodePath => {
+    const { node } = nodePath;
+    const newNode = j.expressionStatement(
+      j.callExpression(j.identifier("run"), [
+        j.arrowFunctionExpression([], j.blockStatement([node]))
+      ])
+    );
+    return newNode;
+  });
+
+  // From this phase we will be combining consecutive 
+  // sequential run loops while maintaining their order
+  let secondRoot = j(firstRoot.toSource());
+
+  let blocksHavingRunLoopExpressions = secondRoot.find(j.CallExpression, {
+    callee: {
+      name: "run"
+    }
+  }).closest(j.BlockStatement);
+
+  blocksHavingRunLoopExpressions.forEach((nodePath) => {
+    let { node } = nodePath;
+    // Below array stores information about where an expression 
+    // is a run loop expression or not, for all expressions inside a block
+    let exArr = []
+
+    node.body.forEach((statement) => {
+      if (statement.expression &&
+        statement.expression.callee &&
+        statement.expression.callee.name === 'run') {
+        exArr.push(true);
+      } else {
+        exArr.push(false);
+      }
+    });
+
+    let batcher = [];
+    exArr.forEach(function (bool, index) {
+      if (bool) {
+        batcher.push(index);
+      } else {
+        transform();
+      }
+    });
+    transform();
+    function transform() {
+      if (batcher.length > 1) {
+        let cNodes = batcher.map((key, index) => {
+          return node.body[key];
+        })
+
+        let mainBlock = cNodes[0].expression.arguments[0].body;
+
+        let newNodes = [...mainBlock.body];
+        cNodes.forEach((currentNode, index) => {
+          if (index !== 0) {
+            newNodes.push(...currentNode.expression.arguments[0].body.body);
+          }
+        });
+
+        mainBlock.body = newNodes;
+
+        batcher.forEach((key, index) => {
+          if (index !== 0) {
+            delete node.body[key];
+          }
+        })
+        isModified = true;
+      }
+      batcher = [];
+    }
+
+    return node;
+  });
+
+
 
   if (isModified && !isImportExists) {
     const importStatement = j.importDeclaration(
       [j.importSpecifier(j.identifier("run"), j.identifier("run"))],
       j.literal("@ember/runloop")
     );
-    root
+    secondRoot
       .find(j.ImportDeclaration)
       .get()
       .insertAfter(importStatement);
   }
 
   return beautifyImports(
-    root.toSource({
+    secondRoot.toSource({
       quote: 'single',
       lineTerminator,
       trailingComma: false
